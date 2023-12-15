7223835f62b7c43d8f11aba840ecb3a7b39826a8
Alexandros Zacharakis <alexandros.zacharakis8@gmail.com>
2023-12-15 13:25:03 +0100

    --amend

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,18 +1,19 @@
-use std::fmt::{Debug, Display, Formatter, Result};
+use std::fmt::{Debug, Display, Formatter, Result as NullResult};
 
-use bls12_381::{G1Projective, Scalar};
+use bls12_381::{G1Projective, Scalar, G1Affine};
+use serde::{Serialize, Deserialize, Deserializer};
 
 #[derive(Clone, PartialEq, Eq, Copy)]
 pub struct Message(pub Scalar);
 
 impl Debug for Message {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> NullResult {
         write!(f, "{:?}", self.0)
     }
 }
 
 impl Display for Message {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> NullResult {
         write!(f, "{:?}", self.0)
     }
 }
@@ -21,13 +22,13 @@ impl Display for Message {
 pub struct Ciphertext(pub G1Projective, pub G1Projective);
 
 impl Debug for Ciphertext {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> NullResult {
         write!(f, "(\n\t{:?}, \n\t{:?}\n)", self.0, self.1)
     }
 }
 
 impl Display for Ciphertext {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> NullResult {
         write!(f, "(\n\t{:?}, \n\t{:?}\n)", self.0, self.1)
     }
 }
@@ -36,13 +37,13 @@ impl Display for Ciphertext {
 pub struct EGPublikKey(pub G1Projective);
 
 impl Debug for EGPublikKey {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> NullResult {
         write!(f, "{:?}", self.0)
     }
 }
 
 impl Display for EGPublikKey {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> NullResult {
         write!(f, "{:?}", self.0)
     }
 }
@@ -51,13 +52,13 @@ impl Display for EGPublikKey {
 pub struct EGSecretKey(pub Scalar);
 
 impl Debug for EGSecretKey {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> NullResult {
         write!(f, "{:?}", self.0)
     }
 }
 
 impl Display for EGSecretKey {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> NullResult {
         write!(f, "{:?}", self.0)
     }
 }
@@ -108,6 +109,42 @@ impl OS2IP for Scalar {
         let mut i = buf[..].to_vec();
         i.reverse();
         Scalar::from_bytes(i.as_slice().try_into().unwrap()).unwrap()
+    }
+}
+
+impl EGPublikKey {
+    pub fn from_bytes<T: AsRef<[u8]>>(bytes: T) -> Self {
+        let bytes = bytes.as_ref();
+        if bytes.len() != 48 {
+            panic!("Invalid length");
+        }
+        let mut buf = [0u8; 48];
+        buf.copy_from_slice(bytes);
+        let g1 = G1Projective::from(G1Affine::from_compressed(&buf).unwrap());
+        EGPublikKey(g1)
+    }
+
+    pub fn to_bytes(&self) -> [u8; 48] {
+        G1Affine::from(self.0).to_compressed()
+    }
+}
+
+impl Serialize for EGPublikKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_bytes(&self.to_bytes())
+    }
+}
+
+impl<'de> Deserialize<'de> for EGPublikKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let bytes: Vec<u8> = Vec::deserialize(deserializer)?;
+        Ok(EGPublikKey::from_bytes(&bytes[..]))
     }
 }
 
